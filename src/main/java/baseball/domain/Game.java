64ea519e77b957a009815b73aa59ab86e2ce39c3
donghee.han <handh0413@gmail.com>
2022-04-13 22:36:36 +0900

    feat: Game 입력값 검증 기능 추가
    입력값 검증 기능
    - 입력값 길이 검증
    - 입력값 타입 검증
    - 입력값 중복 검증

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -3,19 +3,29 @@ package baseball.domain;
 public class Game {
 
     void validateInput(String input) {
-
+        validateInputLength(input);
+        validateInputType(input);
+        validateInputDuplicate(input);
     }
 
     void validateInputLength(String input) {
-
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력값(길이)에 문제가 있습니다.");
+        }
     }
 
     void validateInputType(String input) {
-
+        if (!input.matches("[1-9]{3}")) {
+            throw new IllegalArgumentException("입력값(타입)에 문제가 있습니다.");
+        }
     }
 
     void validateInputDuplicate(String input) {
-        
+        if (input.charAt(0) == input.charAt(1)
+            || input.charAt(1) == input.charAt(2)
+            || input.charAt(0) == input.charAt(2)) {
+            throw new IllegalArgumentException("입력값(중복)에 문제가 있습니다.");
+        }
     }
 
 }
