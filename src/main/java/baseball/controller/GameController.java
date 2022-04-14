ae85c1020efdce9b4d489c3271fd82be9cae84b7
donghee.han <handh0413@gmail.com>
2022-04-15 00:51:51 +0900

    feat: GameController 기능 추가
    야구 게임 기능 추가
    - 유저 입력 화면 기능
    - 야구 숫자 초기화 기능

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -3,6 +3,7 @@ package baseball.controller;
 import baseball.domain.Game;
 import baseball.domain.InputResult;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameController {
     private static final String CONTINUE = "1";
@@ -22,12 +23,49 @@ public class GameController {
     }
 
     public String readInput() {
-        System.out.print("TEST: ");
+        System.out.print("숫자를 입력해주세요: ");
         return Console.readLine();
     }
 
     public String makeAnswer() {
-        return "TEST";
+        String answer = "";
+        while (answer.length() != 3) {
+            answer = addDigit(answer);
+        }
+        return answer;
+    }
+
+    private String addDigit(String number) {
+        String digit = String.valueOf(Randoms.pickNumberInRange(1, 9));
+        if (number.contains(digit)) {
+            return number;
+        }
+        return number.concat(digit);
+    }
+
+    public boolean isGameEnd(InputResult result) {
+        String userInput = "";
+        if (result.isSuccess()) {
+            userInput = getUserFinalInput();
+        }
+
+        if (STOP.equals(userInput)) {
+            return true;
+        }
+
+        if (CONTINUE.equals(userInput)) {
+            initAnswer();
+        }
+
+        return false;
+    }
+
+    private String getUserFinalInput() {
+        String userInput;
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        userInput = Console.readLine();
+        return userInput;
     }
 
 }
