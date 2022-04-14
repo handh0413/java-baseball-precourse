c29d255ff82dacf474ec16d168f49f5d3006008a
donghee.han <handh0413@gmail.com>
2022-04-14 21:56:02 +0900

    test: GameTest 테스트 추가
    스트라이크, 볼 개수 테스트
    - 스트라이크 유무 확인
    - 스트라이크 개수 확인
    - 볼 유무 확인
    - 볼 개수 확인

--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -1,81 +1,77 @@
 package baseball.domain;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest {
+    private Game game;
+
+    @BeforeEach
+    public void beforeEach() {
+        game = new Game();
+    }
 
     @Test
     public void 입력_검증_테스트_세자리() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputLength("");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputLength("1");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputLength("12");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputLength("1234");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputLength("123456789");
         });
 
-        Assertions.assertDoesNotThrow(() -> {
-            Game game = new Game();
+        assertDoesNotThrow(() -> {
             game.validateInputLength("123");
         });
     }
 
     @Test
     public void 입력_검증_테스트_타입() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputType("12A");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputType("23#");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputType("AB3");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputType("@#3");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputType("ABC");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputType("#$%");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             Game game = new Game();
             game.validateInputType("012");
         });
 
-        Assertions.assertDoesNotThrow(() -> {
+        assertDoesNotThrow(() -> {
             Game game = new Game();
             game.validateInputType("234");
         });
@@ -83,57 +79,111 @@ public class GameTest {
 
     @Test
     public void 입력_검증_테스트_중복() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputDuplicate("111");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputDuplicate("112");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputDuplicate("122");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInputDuplicate("313");
         });
 
-        Assertions.assertDoesNotThrow(() -> {
-            Game game = new Game();
+        assertDoesNotThrow(() -> {
             game.validateInputDuplicate("345");
         });
     }
 
     @Test
     public void 입력_검증_테스트() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInput("12");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInput("12A");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInput("340");
         });
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
+        assertThrows(IllegalArgumentException.class, () -> {
             game.validateInput("122");
         });
 
-        Assertions.assertDoesNotThrow(() -> {
-            Game game = new Game();
+        assertDoesNotThrow(() -> {
             game.validateInput("456");
         });
+    }
+
+    @Test
+    public void 스트라이크_존재확인() {
+        game.setAnswer("123");
+        assertEquals(1, game.compareForStrike("145", 0));
+        assertEquals(0, game.compareForStrike("145", 1));
+        assertEquals(0, game.compareForStrike("145", 2));
+    }
+
+    @Test
+    public void 볼_존재확인() {
+        game.setAnswer("123");
+        assertEquals(0, game.compareForBall("532", 0));
+        assertEquals(1, game.compareForBall("532", 1));
+        assertEquals(1, game.compareForBall("532", 2));
+
+        game.setAnswer("123");
+        assertEquals(1, game.compareForBall("321", 0));
+        assertEquals(0, game.compareForBall("321", 1));
+        assertEquals(1, game.compareForBall("321", 2));
+    }
+
+    @Test
+    public void 스트라이크_개수확인() {
+        game.setAnswer("123");
+        assertEquals(3, game.calculateStrikes("123"));
+        assertEquals(2, game.calculateStrikes("129"));
+        assertEquals(1, game.calculateStrikes("189"));
+        assertEquals(0, game.calculateStrikes("456"));
+    }
+
+    @Test
+    public void 볼_개수확인() {
+        game.setAnswer("123");
+        assertEquals(3, game.calculateBalls("231"));
+        assertEquals(2, game.calculateBalls("219"));
+        assertEquals(1, game.calculateBalls("139"));
+        assertEquals(0, game.calculateBalls("456"));
+    }
+
+    @Test
+    public void 스트라이크_볼_개수확인() {
+        game.setAnswer("123");
+        InputResult result = game.calculate("123");
+        assertEquals(3, result.getStrikes());
+        assertEquals(0, result.getBalls());
+        assertEquals(true, result.isSuccess());
+
+        game.setAnswer("713");
+        result = game.calculate("145");
+        assertEquals(0, result.getStrikes());
+        assertEquals(1, result.getBalls());
+        assertEquals(false, result.isSuccess());
+
+        result = game.calculate("671");
+        assertEquals(0, result.getStrikes());
+        assertEquals(2, result.getBalls());
+        assertEquals(false, result.isSuccess());
+
+        result = game.calculate("216");
+        assertEquals(1, result.getStrikes());
+        assertEquals(0, result.getBalls());
+        assertEquals(false, result.isSuccess());
     }
 }
