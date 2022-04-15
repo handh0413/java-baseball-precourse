4f0c66979491316c6ce3209dbc3099b81d126d71
donghee.han <handh0413@gmail.com>
2022-04-15 19:57:29 +0900

    test: Application 테스트 추가
    테스트 시나리오 추가
    - 게임종료_1판
    - 게임종료_1스트라이크_2스트라이크_3스트라이크
    - 게임종료_1볼_2볼_3볼_3스트라이크
    - 게임종료_3판

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,6 +9,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 게임종료_1판() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 게임종료_1스트라이크_2스트라이크_3스트라이크() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("167", "137", "135", "2");
+                    assertThat(output()).contains("1스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 게임종료_1볼_2볼_3볼_3스트라이크() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("216", "316", "351", "135", "2");
+                    assertThat(output()).contains("1볼", "2볼", "3볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -17,6 +51,17 @@ class ApplicationTest extends NsTest {
                     assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    void 게임종료_3판() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("135", "1", "246", "1", "357", "2");
+                    assertThat(output()).contains("3스트라이크", "3스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5, 2, 4, 6, 3, 5, 7
         );
     }
 
